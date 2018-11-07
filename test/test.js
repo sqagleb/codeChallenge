fe4ee4ab26bfad7a7d59403e4fa7a55d6ef6aefa
Gleb Shmukler <sqa.gleb@gmail.com>
2018-11-07 14:08:42 -0800

    updated first test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
 //const assert = require('assert');
-const data = require('../data/data').indexPage;
-const locator = require('../data/locators');
+const data = require('../data/data.js').indexPage;
+const locator = require('../data/locator.js');
 
 describe('Testing Search functionality works as intended', function () {
     it('Inputting valid sku', function(){
         browser.url('/');
         browser.waitForExist(locator.indexPage.searchField);
-        browser.setValue(locator.indexPage.searchField,data.indexPage.validSku);
+        console.log(data.validSku);
+        browser.setValue(locator.indexPage.searchField,data.validSku);
+        browser.submitForm(locator.indexPage.searchField);
         browser.pause(5000)
     })
 });
