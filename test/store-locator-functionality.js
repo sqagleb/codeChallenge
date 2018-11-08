bccb0de7c84c4a702eced59e93108faa6e0e83a4
Gleb Shmukler <sqa.gleb@gmail.com>
2018-11-08 03:02:33 -0800

    added test for Texas & uncommented search-functionality.js in wdio.config

--- test/store-locator-functionality.js
+++ test/store-locator-functionality.js
@@ -3,22 +3,21 @@ const data = require('../data/data.js').storeLocatorPage;
 const locator = require('../data/locator.js');
 
 describe('Store locator functionality:', function () {
-    it(`Selecting ${data.testState1} should give 2 stores result`, function(){
+    it(`Selecting ${data.testState1} should give ${data.state1Stores} stores result`, function(){
         this.timeout(50000);
         browser.url('https://www.westelm.com/customer-service/store-locator.html?cm_re=GlobalLinks-_-Footer-_-FindAStore#viewStoreList');
-        // browser.pause(7000);
         browser.click(locator.overlayPage.closeButton);
         browser.waitForVisible(locator.storeLocatorPage.allStoresButton);
         browser.click(locator.storeLocatorPage.allStoresButton);
         browser.waitForVisible(locator.storeLocatorPage.stateSelector);
         browser.selectByVisibleText(locator.storeLocatorPage.stateSelector, data.testState1);
-        browser.pause(3000);
         let arrOfStores = $$(data.storeTitle).length - 5;
-        // chai.assert.equal(skuInUrl, data.validSku,'You are at the wrong page');
+        chai.assert.equal(arrOfStores, data.state1Stores,'Wrong count of stores');
     });
 
-    // it('Image should be visible', function() {
-    //     let imgIsVisisble = browser.isVisible(locator.productPage.imgViewer);
-    //     chai.assert.isTrue(imgIsVisisble,'Image is NOT visible');
-    // })
+    it(`Selecting ${data.testState2} should give ${data.state2Stores} stores result`, function(){
+        browser.selectByVisibleText(locator.storeLocatorPage.stateSelector, data.testState2);
+        let arrOfStores = $$(data.storeTitle).length - 5;
+        chai.assert.equal(arrOfStores, data.state2Stores,'Wrong count of stores');
+    });
 });
