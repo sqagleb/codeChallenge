1d7679fc4f997b18e251332467f368506e793dba
Gleb Shmukler <sqa.gleb@gmail.com>
2018-11-07 23:40:29 -0800

    update

--- data/locator.js
+++ data/locator.js
@@ -2,5 +2,10 @@ module.exports = {
     indexPage: {
         searchField: '#search-field',
         searchButton: '.icon-search'
+    },
+
+    productPage: {
+        imgViewer: '#heroInspectorForFOZ',
+        productName: '.pip-summary'
     }
 };
--- test/test.js
+++ test/test.js
@@ -1,14 +1,18 @@
-//const assert = require('assert');
+const chai = require('chai');
 const data = require('../data/data.js').indexPage;
 const locator = require('../data/locator.js');
 
 describe('Testing Search functionality works as intended', function () {
+    this.timeout(50000);
+    browser.url('/');
+    browser.waitForVisible(locator.indexPage.searchField, 2000);
+    browser.setValue(locator.indexPage.searchField,data.validSku);
+    browser.submitForm(locator.indexPage.searchField);
+    browser.waitForExist(locator.productPage.imgViewer);
     it('Inputting valid sku', function(){
-        browser.url('/');
-        browser.waitForExist(locator.indexPage.searchField);
-        console.log(data.validSku);
-        browser.setValue(locator.indexPage.searchField,data.validSku);
-        browser.submitForm(locator.indexPage.searchField);
-        browser.pause(5000)
+        let imgViewerIsVisible = browser.isVisible(locator.productPage.imgViewer);
+        // let test = browser.getText(locator.productPage.productName);
+        // console.log(test);
+        chai.assert.isTrue(imgViewerIsVisible,'ImgViewer is NOT visible');
     })
 });
